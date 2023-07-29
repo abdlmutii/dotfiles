8e3c6d5a0014e835984770cba6c1670ca12c8861
Abdlmu'tii <92731667+abdlmutii@users.noreply.github.com>
2023-07-29 16:17:20 +0300

    Update init.lua

--- .config/nvim/custom/init.lua
+++ .config/nvim/custom/init.lua
@@ -1,1 +1,17 @@
+-- Enable autosave in Vim.
+vim.g.autosave = true
 
+-- Register the 'html' language with the 'ejs' parser in Vim's treesitter plugin.
+vim.treesitter.language.register('html', 'ejs')
+
+-- Set the fold level to 99 in Vim. This means that all folds will be fully open.
+vim.opt.foldlevel = 99;
+
+-- ( Not recommended ) Configure Vim's diagnostic feature to disable virtual text, which means no inline error/warning messages.
+-- vim.diagnostic.config({ virtual_text = false })
+
+-- Load the custom status column configuration from the "custom.statuscolumn" module.
+require "custom.statuscolumn"
+
+-- Load the custom autocmd configuration from the "custom.autocmd" module.
+require "custom.autocmd"
