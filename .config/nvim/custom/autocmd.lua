fd932cfef5e8bc53c0e18c784184200741a33555
Abdlmu'tii <92731667+abdlmutii@users.noreply.github.com>
2023-07-29 16:22:49 +0300

    Create autocmd.lua

---
+++ .config/nvim/custom/autocmd.lua
@@ -0,0 +1,27 @@
+-- Create an autocmd that triggers on "InsertLeave" and "TextChanged" events.
+vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
+  -- Group the autocmd under the name "Autosave".
+  group = vim.api.nvim_create_augroup("Autosave", {}),
+  -- Define the callback function for the autocmd.
+  callback = function()
+    -- Check if autosave is enabled, the buffer has a name, and it's listed.
+    if vim.g.autosave and #vim.api.nvim_buf_get_name(0) ~= 0 and vim.bo.buflisted then
+      -- If conditions are met, silently write the buffer to save changes.
+      vim.cmd "silent w"
+    end
+  end,
+})
+
+-- Create an autocmd that triggers on "BufWritePre" event for specific file types.
+vim.api.nvim_create_autocmd("BufWritePre", {
+  -- Command to run when the event is triggered - in this case, format the buffer using LSP.
+  command = "lua vim.lsp.buf.format()",
+  -- The pattern specifies filetypes for which the autocmd should trigger.
+  pattern = "*.cpp,*.css,*.go,*.h,*.html,*.js,*.json,*.jsx,*.lua,*.md,*.py,*.rs,*.ts,*.tsx,*.yaml,*.c,*.dart",
+})
+
+-- Create a user-defined command called "Autosave".
+vim.api.nvim_create_user_command("Autosave", function()
+  -- Toggle the global variable "autosave" when the command is executed.
+  vim.g.autosave = not vim.g.autosave
+end, {})
