997b91104fdf30a4c5e6340666cf786284e4db54
Abdlmu'tii <92731667+abdlmutii@users.noreply.github.com>
2023-07-29 16:40:52 +0300

    Delete .config/custom directory

--- .config/custom/init.lua
+++
@@ -1,7 +0,0 @@
-vim.g.autosave = true;
-vim.treesitter.language.register('html', 'ejs');
-vim.opt.foldlevel = 99;
-vim.diagnostic.config({ virtual_text = false })
-
-require "custom.statuscolumn"
-require "custom.autocmd"
